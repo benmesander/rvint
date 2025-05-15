a421ee4221ffc6074f8f05c5a373091b9cd3f865
Ben Mesander <benmesander@yahoo.com>
2025-05-15 12:07:04 -0600

    Begin implementation of convert to hex ascii

--- src/io.s
+++ src/io.s
@@ -9,16 +9,26 @@
 	#
 	# output
 	# a0 - address of null-terminated buffer with output
-	
 to_hex:
-	# isolate byte of interest
-	addi	a1, a1, -1	# zero-base count to an index
-	li	t0, 0xff
-	slli	t1, a1, 3	# t1 = a1 * 8
-	sll	t0, t0, t1
-
-
-
+	la	t0, iobuf
+	slli	a1, a1, 2	# count of nibbles
+	beqz	a2, to_hex_loop
+	li	t1, 0x7830	# '0x' in ascii, little-endian
+	sw	t1, 0(t0)
+	addi	t0, t0, 2
+	
+to_hex_loop:	
+	addi	a1, a1, -1
+	slli	t1, a1, 2
+	srl	t2, a0, t1
+	andi	t2, t2, 0xf
+	addi	t2, t2, '0'
+	sb	t2, 0(t0)
+	addi	t0, t0, 1
+	bnez	a1, to_hex_loop
+	sb	zero, 0(t0)	# nul terminate
+	la	a0, iobuf
+	ret
 .bss
 
 .comm iobuf, 65	# reserves 65 bytes for an i/o buffer
