7ec248b7d3c06f115f1475382c3d57383d728298
Ben Mesander <benmesander@yahoo.com>
2025-05-15 19:05:21 -0600

    - Hack around with register usage; this one needs a stack frame as   it calls nmul

--- src/sqrt.s
+++ src/sqrt.s
@@ -7,34 +7,53 @@
 # a0 - output
 
 isqrt:
-	li	t1, 2
-	blt	a0, t1, isqrt_skip
+	FRAME	4
+	PUSH	ra, 0
+	PUSH	s0, 1
+	PUSH	s1, 2
+	PUSH	s2, 3
+	
+	li	t0, 2
+	blt	a0, t0, isqrt_skip
 
 	# compute using digit by digit algorithm, https://en.wikipedia.org/wiki/Integer_square_root
 	# register usage:
 	# a0 - n
-	# t0 - shift
-	# t1 - temporary
-	# t2 - large_cand
-	# t3 - result
+	# 
+	# t0 - scratch
+	# s0 - shift
+	# s1 - large_cand
+	# s2 - result
 
-	li	t0, 2
+	li	s0, 2
 isqrt_shift_loop:
-	srl	t1, a0, t0
-	bnez	t1, isqrt_bit_setting
-	addi	t0, t0, 2
+	srl	t0, a0, s0
+	bnez	t0, isqrt_bit_setting
+	addi	s0, s0, 2
 	j	isqrt_shift_loop
 
+# fix registers below
+
 isqrt_bit_setting:	
-	li	t3, 0
+	li	s2, 0
 isqrt_bit_setting_loop:
-	bge	t0, zero, xxx
+	bge	s0, zero, isqrt_done
+	slli	s2, s2, 1
+	addi	s1, s2, 1
+
+	# xxx: set up stuff
+	call	nmul
 	
 
 
 isqrt_done:	
-	mv	a0, t3
+	mv	a0, s2
 isqrt_skip:
+	POP	ra, 0
+	POP	s0, 1
+	POP	s1, 2
+	POP	s2, 3
+	EFRAME	4
 	ret
 
 .size isqrt, .-isqrt
