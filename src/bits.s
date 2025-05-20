7977a1fcaab2d6e9a461223c87b2cd2266106cc5
Ben Mesander <benmesander@yahoo.com>
2025-05-20 22:29:08 +0000

    fix count trailing zero bugs

--- src/bits.s
+++ src/bits.s
@@ -11,7 +11,7 @@
 bits_ctz:
 	beqz	a0, bits_ctz_is_zero
 	mv	t2, zero	# result
-	
+
 .if CPU_BITS == 64
 	li	t1, -1
 	srli	t1, t1, 32
@@ -41,33 +41,60 @@ bits_ctz_lower_4:
 	srli	a0, a0, 4
 
 bits_ctz_lower_2:
-	andi	t0, t0, 0x3
+	andi	t0, a0, 0x3
 	bnez	t0, bits_ctz_lower_1
 	addi	t2, t2, 2
 	srli	a0, a0, 2
 
 bits_ctz_lower_1:
-	andi	a0, a0, 0x1
+	andi	t0, a0, 0x1
 	bnez	t0, bits_ctz_done
 	addi	t2, t2, 1
 
-
-bits_ctz_done:	
+bits_ctz_done:
 	mv	a0, t2
 	ret
 
 bits_ctz_is_zero:
-	li	a0, 32
+	li	a0, CPU_BITS
 	ret
 
 .size	bits_ctz, .-bits_ctz
-	
-# count leading zeroes
+
+# count leading zeroes via binary search (use for processors with no B extension)
 # input
 # a0 number
 # output
 # a0 result
 
 bits_clz:
+	beqz	a0, bits_clz_is_zero
+	mv	t2, zero	# result
+
+.if CPU_BITS == 64
+	srli	t0, a0, 32
+	bnez	t0, bits_clz_upper_16
+	addi	t2, t2, 32
+
+
+
+	li	t1, -1
+	slli	t1, t1, 32
+	and	t0, a0, t1
+	bnez	t0, bits_clz_upper_16
+	addi	t2, t2, 32
+	srli	a0, a0, 32
+.endif
+
+
+
+bits_clz_is_zero:
+	mv	a0, t2
 	ret
+
+bits_clz_is_zero:
+	li	a0, 32
+	ret
+
+
 .size	bits_clz, .-bits_clz
