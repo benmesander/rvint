9da2ce1020843336b6e1dd5c95d189f2e5aa3d7f
Benard Mesander <ben@Benards-MacBook-Pro.local>
2025-05-29 07:13:34 -0600

    fix dat-tests linkage, improve gcd register usage

--- src/gcd.s
+++ src/gcd.s
@@ -18,11 +18,11 @@
 # a0 = gcd(u, v)
 ################################################################################
 gcd:
-	FRAME	4
+	# Use s0 and s1 for our main variables
+	FRAME	3
 	PUSH	ra, 0
 	PUSH	s0, 1
 	PUSH	s1, 2
-	PUSH	s2, 3
 
 	beqz	a0, gcd_return_v
 	beqz	a1, gcd_return_u
@@ -32,24 +32,21 @@ gcd:
 
 gcd_loop:			
 	bltu	s0, s1, gcd_skip_swap
-	mv	a3, s1
-	mv	s1, s0		# register swap s0 <> s1
-	mv 	s0, a3
+	mv	t0, s0		# Save u
+	mv	s0, s1		# u = v
+	mv	s1, t0		# v = old u
 
 gcd_skip_swap:	
 	sub	s1, s1, s0	# v -= u
-	beqz	s1, gcd_done
-	j 	gcd_loop
+	bnez	s1, gcd_loop	# Continue if v != 0
 	
-gcd_done:
-	mv	a0, s0		# result = u
+	mv	a0, s0		# Return u
 
 gcd_cleanup:	
 	POP	ra, 0
 	POP	s0, 1
 	POP	s1, 2
-	POP	s2, 3
-	EFRAME	4	
+	EFRAME	3
 	ret
 
 gcd_return_v:
@@ -73,45 +70,33 @@ gcd_return_u:
 # a0 = lcm(u,v)
 ################################################################################
 lcm:
-	FRAME	5
+	FRAME	3
 	PUSH	ra, 0
 	PUSH	s0, 1
 	PUSH	s1, 2
-	PUSH	s2, 3
-	PUSH	s3, 4
 
 	beqz	a0, lcm_check_a1	# check to see if both a0 and a1 are 0
 
 lcm_start:	
-	bltu	a0, a1, lcm_skip_swap
+	mv	s0, a0		# Save first number
+	mv	s1, a1		# Save second number
 
-	mv	a3, a0
-	mv	a0, a1		# register swap a0 <> a1
-	mv 	a1, a3
-
-lcm_skip_swap:	
-	mv	s0, a0		# Save original a0
-	mv	s1, a1		# Save original a1
-
+	# Calculate GCD first
 	jal	gcd		# gcd result in a0
-	mv	s2, a0		# Save GCD result
+	mv	a1, a0		# Move GCD to divisor register
+	mv	a0, s0		# First number
+	call	divremu		# Divide first number by GCD
+	mv	t0, a0		# Save quotient temporarily
 
-	mv	a0, s0		# Restore original a0
-	mv	a1, s2		# Use GCD result as divisor
-	call	divremu		# result in a0
-	mv	s3, a0		# Save division result
-
-	mv	a0, s3		# Use division result
-	mv	a1, s1		# Use original a1
-	call	nmul		# result in a0
+	mv	a0, t0		# Load saved quotient
+	mv	a1, s1		# Second number
+	call	nmul		# Multiply to get LCM
 
 lcm_cleanup:	
-	POP 	ra, 0
+	POP	ra, 0
 	POP	s0, 1
 	POP	s1, 2
-	POP	s2, 3
-	POP	s3, 4
-	EFRAME	5
+	EFRAME	3
 	ret
 
 lcm_check_a1:
