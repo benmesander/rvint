bdb09f586255dfd23b7b3096b5a100e93138567c
Ben Mesander <benmesander@yahoo.com>
2025-05-30 16:15:57 +0000

    revert changes

--- src/alg-tests.s
+++ src/alg-tests.s
@@ -1,256 +1,355 @@
 .include "config.s"
+
 .text
-.globl	_start
-.globl	print
+	
+.globl _start
 _start:
-	# test 0 - print empty table
-	la	a1, test
-	li	a2, 5
-	jal	print
 	li	a0, 0
-	call	to_decu
-	mv	a2, a1
-	mv	a1, a0
-	jal	print
-	la	a1, nl
-	li	a2, 1
-	jal	print
-	jal	print_hash_table
+	li	a1, 9
+	li	a2, 3
+	call	sqrt_test
 
-	# test 1 - insert a key/value
-	la	a0, oink
-	la	a1, bar
-	call	hash_insert
-
-#	la	a0, oink
-#	call	sum_key
-#	call	to_decu
-#	mv	a2, a1
-#	mv	a1, a0
-#	call	print
-
-
-	la	a1, test
-	li	a2, 5
-	jal	print
+.if CPU_BITS == 64
 	li	a0, 1
-	call	to_decu
-	mv	a2, a1
-	mv	a1, a0
-	jal	print
-	la	a1, nl
-	li	a2, 1
-	jal	print
-	jal	print_hash_table
+	li	a1, 92423004303
+	li	a2, 304011
+	call	sqrt_test
+.endif
 
-	# test 2 - insert a 2nd key/value and a collision key/value
-
-	la	a0, foo
-	la	a1, bar
-	call	hash_insert
-	
-	la	a0, knio
-	la	a1, foo
-	call	hash_insert
-
-	la	a1, test
-	li	a2, 5
-	jal	print
 	li	a0, 2
-	call	to_decu
-	mv	a2, a1
-	mv	a1, a0
-	jal	print
-	la	a1, nl
-	li	a2, 1
-	jal	print
-	jal	print_hash_table
+	li	a1, 40
+	li	a2, 30
+	li	a3, 10
+	call	gcd_test
 
-	# test 3 - insert a 3rd collision, delete 2nd one
+	li	a0, 3
+	li	a1, 2390842
+	li	a2, 4234
+	li	a3, 2
+	call	gcd_test
 
-	la	a0, inko
-	la	a1, foo
-	call	hash_insert
 
-#	la	a0, knio
-#	call	hash_remove
+	li	a0, 4
+	li	a1, 100
+	li	a2, 125
+	li	a3, 500
+	call	lcm_test
 
+	li	a0, 5
+	li	a1, 21
+	li	a2, 6
+	li	a3, 42
+	call	lcm_test
+
+	li	a0, 6
+	li	a1, 0b00010001000100010001000100010001
+.if CPU_BITS == 32
+	li	a2, 3
+.else
+	li	a2, 35
+.endif
+	jal	clz_test
+
+	li	a0, 7
+	li	a1, 0b00000000000000000001000100010001
+.if CPU_BITS == 51
+	li	a2, 19
+.else
+	li	a2, 51
+.endif
+	jal	clz_test
+
+
+
+	j	_end
+
+# test # a0
+header:
+	FRAME	2
+	PUSH	ra, 0
+	PUSH	s0, 1
+	mv	s0, a0
+	
 	la	a1, test
 	li	a2, 5
-	jal	print
-	li	a0, 3
+	call	print
+	
+	mv	a0, s0
 	call	to_decu
 	mv	a2, a1
 	mv	a1, a0
-	jal	print
-	la	a1, nl
-	li	a2, 1
-	jal	print
-	jal	print_hash_table
-
-	j	end
-	
-################################################################################
-# routine: print_hash_table
-#
-# Print the hash table contents in the specified format.
-# For each entry with nonzero flags, prints:
-# <index>: <flags>:<key_ptr>(<string>):<value>
-#
-# Uses only rv32i/rv64i instructions.
-# No input/output registers.
-################################################################################
-print_hash_table:
-	FRAME	6
-	PUSH	ra, 0
-	PUSH	s0, 1			# Entry counter
-	PUSH	s1, 2			# Current entry pointer
-PUSH	s2, 3			# Save flags
-PUSH	s3, 4			# Save key pointer
-PUSH	s4, 5			# Save value
-
-# Print header
-la	a1, header_str
-li	a2, 28			# Length of header string
-jal	print
-
-# Initialize
-li	s0, 0			# Entry counter = 0
-la	s1, hash_table		# Current entry pointer = hash_table
-
-print_hash_loop:
-# Check if we've processed all entries
-li	t0, HASHENTRIES
-beq	s0, t0, print_hash_done
-
-# Load flags
-lw	s2, FLAGSOFFSET(s1)
-beqz	s2, print_hash_next	# Skip if flags are zero
-
-# Print entry number
-	mv	a0, s0
-	jal	to_decu			# Convert to decimal string
-	mv	a2, a1			# Save length in a2
-	mv	a1, a0			# Move string pointer to a1 for print
-	jal	print
-
-	# Print ": "
-	la	a1, colon_space
+	call	print
+	la	a1, colon
 	li	a2, 2
-	jal	print
+	call	print
 
-	# Print flags
-	andi	t0, s2, FLAG_TOMBSTONE
-	beqz	t0, print_no_t
-	la	a1, tomb
-	j	print_t_done
-print_no_t:
-	la	a1, space_char
-print_t_done:
+	POP	ra, 0
+	POP	s0, 1
+	EFRAME	2
+	ret
+
+# a0 test #
+# a1 number
+# a2 expected result
+	
+sqrt_test:
+	FRAME	5
+	PUSH	ra, 0
+	PUSH	s0, 1
+	PUSH	s1, 2
+	PUSH	s2, 3
+	PUSH	s3, 4
+	mv	s0, a0
+	mv	s1, a1
+	mv	s2, a2	# expected result
+
+	call	header
+
+	mv	a0, s1
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, equals
+	li	a2, 3
+	call	print
+
+	mv	a0, s1
+	call	isqrt
+	mv 	s3, a0		# caculated result
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, space
 	li	a2, 1
-	jal	print
+	call	print
 
-	andi	t0, s2, FLAG_INUSE
-	beqz	t0, print_no_i
-	la	a1, inuse
-	j	print_i_done
-print_no_i:
-	la	a1, space_char
-print_i_done:
+	sub	s2, s2, s3
+	bnez	s2, sqrt_test_fail
+	la	a1, pass
+	
+sqrt_test_done:	
+	li	a2, 5
+	call	print
+	
+	POP	ra, 0
+	POP	s0, 1
+	POP	s1, 2
+	POP	s2, 3
+	POP	s3, 4
+	EFRAME	5
+	ret
+
+sqrt_test_fail:
+	la	a1, fail
+	j	sqrt_test_done
+
+
+# input
+# a0 test #
+# a1 u
+# a2 v
+# a3 expected gcd
+gcd_test:
+
+	FRAME	5
+	PUSH	ra, 0
+	PUSH	s0, 1
+	PUSH	s1, 2
+	PUSH	s2, 3
+	PUSH	s3, 4
+	mv	s1, a1
+	mv	s2, a2
+	mv 	s3, a3
+
+	call	header
+
+	mv	a0, s1
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, space
 	li	a2, 1
-	jal	print
+	call	print
 
-	# Print colon
-	la	a1, colon
+	mv	a0, s2
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, equals
+	li	a2, 3
+	call	print
+
+	mv	a0, s1
+	mv	a1, s2
+	call	gcd
+	mv	s2, a0
+
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, space
 	li	a2, 1
-	jal	print
+	call	print
 
-	# Print key pointer in hex
-.if CPU_BITS == 64
-	ld	s3, KEYOFFSET(s1)
-	li	a1, 8			# 8 bytes for 64-bit
-.else
-	lw	s3, KEYOFFSET(s1)
-	li	a1, 4			# 4 bytes for 32-bit
-.endif
-	mv	a0, s3
-	li	a2, 1			# Include 0x prefix
-	jal	to_hex			# Convert to hex string
-	mv	a2, a1			# Save length in a2
-	mv	a1, a0			# Move string pointer to a1 for print
-	jal	print
+	sub	s2, s2, s3
+	bnez	s2, gcd_test_fail
+	la	a1, pass
 
-	# Print key string in parentheses
-	la	a1, open_paren
-	li	a2, 1
-	jal	print
-	mv	a1, s3			# Key string pointer
-	mv	a0, s3
-	jal	strlen			# Get string length
-	mv	a2, a0			# Length for print
-	jal	print
-	la	a1, close_paren
-	li	a2, 1
-	jal	print
-
-	# Print colon
-	la	a1, colon
-	li	a2, 1
-	jal	print
-
-	# Print value in hex
-.if CPU_BITS == 64
-	ld	s4, VALOFFSET(s1)
-	li	a1, 8			# 8 bytes for 64-bit
-.else
-	lw	s4, VALOFFSET(s1)
-	li	a1, 4			# 4 bytes for 32-bit
-.endif
-	mv	a0, s4
-	li	a2, 1			# Include 0x prefix
-	jal	to_hex			# Convert to hex string
-	mv	a2, a1			# Save length in a2
-	mv	a1, a0			# Move string pointer to a1 for print
-	jal	print
-
-	# Print newline
-	la	a1, nl
-	li	a2, 1
-	jal	print
-
-print_hash_next:
-	addi	s0, s0, 1		# Increment counter
-	addi	s1, s1, ELEMENTLEN	# Move to next entry
-	j	print_hash_loop
-
-print_hash_done:
-	# Print footer
-	la	a1, footer_str
-	li	a2, 26			# Length of footer string
-	jal	print
+gcd_test_done:
+	li	a2, 5
+	call	print
 
 	POP	ra, 0
 	POP	s0, 1
 	POP	s1, 2
 	POP	s2, 3
 	POP	s3, 4
-	POP	s4, 5
-	EFRAME	6
+	EFRAME	5
 	ret
 
-# in: a0 - ptr to string
-# out: a0 - count
-# clobbers a0, t0, t1
-strlen:
-	mv	t0, a0
-strlen_loop:	
-	lb	t1, 0(a0)
-	beqz	t1, strlen_done
-	addi	a0, a0, 1
-	j	strlen_loop
-strlen_done:
-	sub	a0, a0, t0
+gcd_test_fail:
+	la	a1, fail
+	j	gcd_test_done
+	
+# input
+# a0 test #
+# a1 u
+# a2 v
+# a3 expected lcm
+lcm_test:
+
+	FRAME	5
+	PUSH	ra, 0
+	PUSH	s0, 1
+	PUSH	s1, 2
+	PUSH	s2, 3
+	PUSH	s3, 4
+	mv	s1, a1
+	mv	s2, a2
+	mv 	s3, a3
+
+	call	header
+
+	mv	a0, s1
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, space
+	li	a2, 1
+	call	print
+
+	mv	a0, s2
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, equals
+	li	a2, 3
+	call	print
+
+	mv	a0, s1
+	mv	a1, s2
+	call	lcm
+	mv	s2, a0
+
+	call	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+
+	la	a1, space
+	li	a2, 1
+	call	print
+
+	sub	s2, s2, s3
+	bnez	s2, lcm_test_fail
+	la	a1, pass
+
+lcm_test_done:
+	li	a2, 5
+	call	print
+
+	POP	ra, 0
+	POP	s0, 1
+	POP	s1, 2
+	POP	s2, 3
+	POP	s3, 4
+	EFRAME	5
 	ret
+
+lcm_test_fail:
+	la	a1, fail
+	j	lcm_test_done
+
+# input a0 test #
+# a1 number
+# a2 leading zeroes	
+clz_test:
+	FRAME	3
+	PUSH	ra, 0
+	PUSH	s0, 1
+	PUSH	s1, 2
+	
+	mv	s0, a1
+	mv	s1, a2
+	
+	jal	header
+
+	mv	a0, s0
+	li	a1, CPU_BYTES
+	li	a2, 1
+	jal	to_bin
+	mv	a2, a1
+	mv 	a1, a0
+	jal	print
+	la	a1, space
+	li	a2, 1
+	jal	print
+
+	mv	a0, s0
+	jal	bits_clz
+
+	mv	s0, a0
+	jal	to_decu
+	mv	a2, a1
+	mv	a1, a0
+	call	print
+	la	a1, space
+	li	a2, 1
+	call	print
+
+
+	sub	a0, s0, s1
+	bnez	a0, clz_test_fail
+	la	a1, pass
+	
+clz_test_done:	
+	li	a2, 5
+	jal	print
+
+	POP	s1, 2
+	POP	s0, 1
+	POP	ra, 0
+	EFRAME 1
+	ret
+
+clz_test_fail:
+	la	a1, fail
+	j	clz_test_done
+
 
 # a1 - ptr to string to print
 # a2 - # bytes to print
@@ -260,25 +359,16 @@ print:
 	ecall
 	ret
 
-end:
+_end:
         li	a0, 0	# exit code
         li	a7, 93	# exit syscall
         ecall
 
-.data
-header_str:	.string "--- start of hash table ---\n"
-footer_str:	.string "--- end of hash table ---\n"
-colon_space:	.string ": "
-inuse:		.string "I"
-tomb:		.string "T"
-space_char:	.string " "
-open_paren:	.string "("
-close_paren:	.string ")"
-nl:		.string "\n"
-colon:		.string ":"
-test:		.string "test "
-oink:		.string "oink"
-knio:		.string "knio"
-inko:		.string "inko"
-foo:		.string "foo"
-bar:		.string "bar"
+test:	.asciz	"test "
+pass:	.asciz	"pass\n"
+fail:	.asciz	"fail\n"
+colon:	.asciz	": "
+x:	.asciz	" x "
+equals:	.asciz	" = "
+nl:	.asciz	"\n"
+space:	.asciz	" "
